98cf15048f1a2d942130efdeb8cf656c08a9db0a
Alexander Hasiba <alexander.hasiba@bulme.at>
2022-04-22 12:40:22 +0200

    Abgabe

--- counterwidget.cpp
+++ counterwidget.cpp
@@ -1,18 +1,21 @@
 #include <QHBoxLayout>
+#include <QGridLayout>
 #include "counterwidget.h"
 #include "config.h"
 
 CounterWidget::CounterWidget(QWidget *parent)
-    : QWidget(parent), m_count(0), m_timer(this), m_gpio(this)
+    : QWidget(parent), m_count(0), m_timer(this), m_gpio(this), m_number(0)
 {
     setWindowTitle("BinaryCounter");
     resize(300, 200);
     QHBoxLayout* layout = new QHBoxLayout(this);
     setLayout(layout);
 
+
     // QLCDNumber zur Anzeige des Zaehlerstandes dezimal:
-
-
+    m_LCD = new QLCDNumber(3);
+    layout->addWidget(m_LCD);
+    m_LCD->display(m_number);
 
     connect(&m_timer, &QTimer::timeout, this, &CounterWidget::updateCounter);
     m_timer.start(T_UPDATE);
@@ -26,8 +29,28 @@ CounterWidget::~CounterWidget()
 // Zaehlerstand im Widget und dual ueber LEDs anzeigen:
 void CounterWidget::updateCounter()
 {
+    if(m_number > 15) m_number = 0;
+    if(m_number < 0) m_number = 15;
+    if(m_gpio.isActivated(BUTTONS[2]) )
+    {
+        m_number = (m_number+ 1);
+    }
+    if(m_gpio.isActivated(BUTTONS[0]))
+    {
+        m_number = (m_number - 1) ;
+    }
+    if(m_gpio.isActivated(BUTTONS[1]) )
+    {
+        m_number = 0;
+    }
 
 
+    m_LCD->display(m_number);
+    updLED();
 }
 
-
+void CounterWidget::updLED()
+{
+    for(int i = 0; i < 4; i++)
+        m_gpio.set(LEDS[i], (m_number >> i));
+}
--- counterwidget.h
+++ counterwidget.h
@@ -1,9 +1,10 @@
 #ifndef COUNTERWIDGET_H
 #define COUNTERWIDGET_H
 
-#include <QLCDNumber>
+#include <QWidget>
 #include <QLabel>
 #include <QTimer>
+#include <QLCDNumber>
 #include "gpio.h"
 
 class CounterWidget : public QWidget
@@ -18,9 +19,12 @@ public slots:
     void updateCounter();
 
 private:
+     void updLED();
      int m_count;
      QTimer m_timer;
      Gpio m_gpio;
+     QLCDNumber* m_LCD = nullptr;
+     int m_number;
 
 };
 
